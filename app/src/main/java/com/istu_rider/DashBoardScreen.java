0a736e07896870d4cc028c467a83cd8ced972392
krishnakumar <softeng.krishna@gmail.com>
2015-09-08 17:17:53 +0530

    sfdfsd

--- app/src/main/java/com/istu_rider/DashBoardScreen.java
+++ app/src/main/java/com/istu_rider/DashBoardScreen.java
@@ -1,16 +1,36 @@
 package com.istu_rider;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class DashBoardScreen extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board_screen);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            toolbar.setTitle("Dashboard");
+//            toolbar.setLogo(R.drawable.logo);
+            setSupportActionBar(toolbar);
+        }
+        toolbar.setBackgroundColor(Color.parseColor("#009688"));
+        toolbar.setNavigationIcon(R.drawable.ic_action_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
 
     @Override
